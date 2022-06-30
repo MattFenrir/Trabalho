a5a9956e660613223318a1fa4524ccfc55ed08a2
Mateus <amaral5x@hotmail.com>
2022-06-30 16:54:30 -0300

    JS 1.0

---
+++ script.js
@@ -0,0 +1,53 @@
+function inicio(){
+    document.querySelectorAll('main section')[0].style.display = "block"
+    document.querySelectorAll('main section')[1].style.display = "block"
+    document.querySelectorAll('main section')[2].style.display = "block"
+    document.querySelectorAll('main section')[3].style.display = "block"
+    document.querySelectorAll('main section')[4].style.display = "block"
+    document.querySelectorAll('main section')[5].style.display = "none"
+    document.querySelectorAll('main section')[6].style.display = "none"
+}
+
+function historia(){
+    document.querySelectorAll('main section')[0].style.display = "block"
+    document.querySelectorAll('main section')[1].style.display = "block"
+    document.querySelectorAll('main section')[2].style.display = "block"
+    document.querySelectorAll('main section')[3].style.display = "block"
+    document.querySelectorAll('main section')[4].style.display = "block"
+    document.querySelectorAll('main section')[5].style.display = "none"
+    document.querySelectorAll('main section')[6].style.display = "none"
+}
+
+function personagens(){
+    document.querySelectorAll('main section')[0].style.display = "none"
+    document.querySelectorAll('main section')[1].style.display = "none"
+    document.querySelectorAll('main section')[2].style.display = "none"
+    document.querySelectorAll('main section')[3].style.display = "none"
+    document.querySelectorAll('main section')[4].style.display = "none"
+    document.querySelectorAll('main section')[5].style.display = "block"
+    document.querySelectorAll('main section')[6].style.display = "none"
+}
+
+function wise(){
+    document.querySelectorAll('main section')[0].style.display = "none"
+    document.querySelectorAll('main section')[1].style.display = "none"
+    document.querySelectorAll('main section')[2].style.display = "none"
+    document.querySelectorAll('main section')[3].style.display = "none"
+    document.querySelectorAll('main section')[4].style.display = "none"
+    document.querySelectorAll('main section')[5].style.display = "none"
+    document.querySelectorAll('main section')[6].style.display = "block"
+    document.getElementById('espioes').style.display = "block"
+    document.getElementById('assassinos').style.display = "none"
+}
+
+function garden(){
+    document.querySelectorAll('main section')[0].style.display = "none"
+    document.querySelectorAll('main section')[1].style.display = "none"
+    document.querySelectorAll('main section')[2].style.display = "none"
+    document.querySelectorAll('main section')[3].style.display = "none"
+    document.querySelectorAll('main section')[4].style.display = "none"
+    document.querySelectorAll('main section')[5].style.display = "none"
+    document.querySelectorAll('main section')[6].style.display = "block"
+    document.getElementById('espioes').style.display = "none"
+    document.getElementById('assassinos').style.display = "block"
+}
